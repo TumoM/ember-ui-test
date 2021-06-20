ad23474a7bfdf31344555613bb04ec76ba48d1b0
Tumo M <tumomasire@gmail.com>
2021-06-20 02:14:55 +0200

    Modal and confirmation logic

--- app/controllers/users/user.js
+++ app/controllers/users/user.js
@@ -1,4 +1,37 @@
 import Controller from '@ember/controller';
 
-export default class UsersUserController extends Controller {
-}
+// export default class UsersUserController extends Controller {
+//     actions: {
+//         archive: function() {
+//             console.log("hello");
+//         }
+    
+export default Ember.Controller.extend ({
+    show_modal: false,
+    actions: {
+        close_confirmation() {
+            Ember.Logger.info(`Archiving Cancelled`)
+            this.set("show_modal",false)
+        },
+        confirm_archive(user){
+            Ember.Logger.info(`Archiving User`)
+            user.archived = true
+            user.save()
+            .then((res) => {
+                Ember.Logger.info(`Archived user ${user.id} - ${user.name}`)
+                Ember.Logger.info(`Res ${res}`)
+            })
+            .catch(
+                () => {
+                  Ember.Logger.info('failure to save');
+                }
+              );
+            this.set("show_modal",false)
+        },
+        show_modal() {
+            Ember.Logger.info(`Launching Modal`)
+            this.set("show_modal",true)
+        }
+    }
+    }
+    )
